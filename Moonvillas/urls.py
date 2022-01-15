a187f051c9f9ae1641914edf434aa132290406ad
Your Name <mohammedanvark.talrop@gmail.com>
2022-01-15 15:15:04 +0530

    done

--- Moonvillas/urls.py
+++ Moonvillas/urls.py
@@ -27,4 +27,4 @@ urlpatterns = [
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = 'Moonvillas Administration'
-admin.site.site_title = 'Sit administration | Moonvillas site admin'
+admin.site.site_title = 'Moonvillas site admin'
